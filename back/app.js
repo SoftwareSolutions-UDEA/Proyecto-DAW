099b25a92303b1c302dfb88f7424223937dea899
Tito Riccardo <titoriccardo@gmail.com>
2022-10-07 19:48:35 -0500

    configuración del proyecto y subida de archivos al repositorio

---
+++ back/app.js
@@ -0,0 +1,12 @@
+const express=require("express");
+const app = express();
+
+app.use(express.json());
+
+//Importar rutas
+const productos=require("./routes/products")
+
+app.use('/api',productos) //Sujeto a decision (ruta del navegador)
+
+module.exports=app
+//
